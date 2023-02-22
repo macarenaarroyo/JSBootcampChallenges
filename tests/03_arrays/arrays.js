dc321d1ad2e55d978b5a6dd2fcd07146aaf8a903
macarenaarroyo <macarenaarroyogil@zoho.com>
2023-02-22 18:31:34 -0500

    arrays homework

--- tests/03_arrays/arrays.js
+++ tests/03_arrays/arrays.js
@@ -1,9 +1,26 @@
 /* eslint-disable no-unused-vars */
 function multiply(arr) {
-
+  // set total = 1
+  let total = 1;
+  // loop over the arrary and multiply all numbers in the array
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 }
 
 function includesCopy(arr, searchValue) {
+  //loop through array check if item in array is equal to searchValue
+  // if yes we should return true
+  // if not wait until end of the loop and return false
+  for (let i = 0; i < arr.length; i++) {
+    const repitedValue = arr[i];
+    if (repitedValue === searchValue) {
+      return true;
+    }
+  }
+  return false;
+
 
 }
 
@@ -15,8 +32,16 @@ function camelCase(str) {
 
 }
 
-function joiner() {
-
+function joiner(arr, del = ',') {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      str += arr[i];
+    } else {
+      str += arr[i] + del;
+    }
+  }
+  return str;
 }
 
 module.exports = {
