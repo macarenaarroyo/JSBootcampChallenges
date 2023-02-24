ad44bdd26d169375accabf5fd493456ae90dcada
macarenaarroyo <macarenaarroyogil@zoho.com>
2023-02-24 17:38:12 -0500

    Objects Homework

--- tests/04_objects/objects.js
+++ tests/04_objects/objects.js
@@ -2,15 +2,25 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function deleteProp(obj, prop) {
-
+  delete obj[prop];
+  return obj;
 }
 
 function numObjectProps(obj) {
-
+  let total = 0;
+  for (let key in obj) {
+    total += 1;
+  }
+  return total;
 }
 
 function shoppingCart(cart) {
-
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    const cartItem = cart[i];
+    total += cartItem.amount;
+  }
+  return total;
 }
 
 function compareObjects(obj1, obj2) {
