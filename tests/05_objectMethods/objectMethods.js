7f519694e0e089986069f3139a8d13db6ce43bd1
macarenaarroyo <macarenaarroyogil@zoho.com>
2023-02-26 22:15:12 -0500

    Object methods homework

--- tests/05_objectMethods/objectMethods.js
+++ tests/05_objectMethods/objectMethods.js
@@ -3,10 +3,23 @@
 /* eslint-disable no-undef */
 
 const greet = {
+    firstName: 'Maria',
+    sayHello() {
+        return `Hello, I'm ${this.firstName}`;
+    }
 };
 
 const calc = {
-
+    total: 0,
+    add(num) {
+        return this.total += num;
+    },
+    subtract(num) {
+        return this.total -= num;
+    },
+    clear() {
+        this.total = 0;
+    }
 };
 
 module.exports = { greet, calc };
